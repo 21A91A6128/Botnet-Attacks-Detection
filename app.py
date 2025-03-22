fe85374f66f5fc928fd12fa7dd98d1a74419b9e1
Code-X-Slayer <karthikryali2007@gmail.com>
2025-03-22 11:25:17 +0530

    fix header redirect link in analysis page

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = 'thecodexcipher@gmail.com'
-app.config['MAIL_PASSWORD'] = 'YOURAPPPASSWORD'
+app.config['MAIL_PASSWORD'] = 'upibhbkdvhbclinc'
 mail = Mail(app)
 
 def sendEmailVerificationRequest(message,submsg,receiver,user):
